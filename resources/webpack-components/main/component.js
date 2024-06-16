7db0f15e88bebf526707b1fc61d8eaa9d5e38d60
zdm <zdm@softvisio.net>
2024-06-16 18:57:23 +0300

    fix: vue flags

--- resources/webpack-components/main/component.js
+++ resources/webpack-components/main/component.js
@@ -261,8 +261,12 @@ export default class extends WebpackComponent {
                 } ),
 
                 new webpack.DefinePlugin( {
+
+                    // https://vuejs.org/api/compile-time-flags.html
                     "__VUE_OPTIONS_API__": "true",
                     "__VUE_PROD_DEVTOOLS__": "false",
+                    "__VUE_PROD_HYDRATION_MISMATCH_DETAILS__": "false",
+
                     "process.env": this.webpackProcessEnv,
                     "process._APP_CONFIG_PLACEHOLDER": this.webpackAppConfig,
                 } ),
