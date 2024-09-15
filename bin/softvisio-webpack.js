28d84715de1a992673da08ac18ef1750919d2783
zdm <zdm@softvisio.net>
2024-09-15 14:15:48 +0300

    fix: preserve symlinks

--- bin/softvisio-webpack.js
+++ bin/softvisio-webpack.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
+import childProcess from "node:child_process";
+
 // preserve symlinks
 const execArgv = new Set( process.execArgv );
 
 if ( !( execArgv.has( "--preserve-symlinks" ) || process.env.NODE_PRESERVE_SYMLINKS ) || !( execArgv.has( "--preserve-symlinks-main" ) || process.env.NODE_PRESERVE_SYMLINKS_MAIN ) ) {
-    const { "default": childProcess } = await import( "node:child_process" );
-
     const res = childProcess.spawnSync(
         process.argv[ 0 ],
         [
