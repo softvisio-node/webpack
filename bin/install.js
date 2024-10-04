db5bf5945c7c5146b17a83854e1c075c12e5e2b1
zdm <zdm@softvisio.net>
2024-10-04 15:14:06 +0300

    fix: install script

--- bin/install.js
+++ bin/install.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 
+import Cli from "#core/cli";
 import externalResources from "#core/external-resources";
+
+const CLI = {
+    "title": "Update resources",
+    "options": {
+        "force": {
+            "description": "Force update",
+            "default": false,
+            "schema": {
+                "type": "boolean",
+            },
+        },
+    },
+};
+
+await Cli.parse( CLI );
 
 externalResources.add( {
     "id": "softvisio-node/core/resources/local.softvisio.net",
     "node": true,
 } );
 
-const res = await externalResources.update( {
-    "force": false,
-    "silent": false,
+const res = await externalResources.install( {
+    "force": process.cli.options.force,
 } );
 
 if ( !res.ok ) process.exit( 1 );
